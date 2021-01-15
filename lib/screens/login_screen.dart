408a097e3aa7235c0f9b2ee6a05d8bcba55453e8
Kamal Morjal <kamal@morjal.com>
2021-01-15 07:07:20 +0530

    wrap new build context and ... (Closes #14)
    - remove spinner when response is received from the firebase server
    - show a snackbar notification when there is a known error: wrong
      password
    
    see docs for more about the wrapping:
    https://api.flutter.dev/flutter/material/Scaffold/of.html

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -21,88 +21,109 @@ class _LoginScreenState extends State<LoginScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: ModalProgressHUD(
-        inAsyncCall: _showspinner,
-        child: Padding(
-          padding: EdgeInsets.symmetric(horizontal: 24.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              Flexible(
-                child: Hero(
-                  tag: 'logo',
-                  child: Container(
-                    height: 200.0,
-                    child: Image.asset('images/logo.png'),
+      body: Builder(
+        builder: (BuildContext context) {
+          return ModalProgressHUD(
+            inAsyncCall: _showspinner,
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 24.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: <Widget>[
+                  Flexible(
+                    child: Hero(
+                      tag: 'logo',
+                      child: Container(
+                        height: 200.0,
+                        child: Image.asset('images/logo.png'),
+                      ),
+                    ),
                   ),
-                ),
-              ),
-              SizedBox(
-                height: 48.0,
-              ),
-              TextField(
-                textAlign: TextAlign.center,
-                keyboardType: TextInputType.emailAddress,
-                onChanged: (value) {
-                  email = value;
-                },
-                style: TextStyle(color: Colors.black),
-                decoration: kTextFieldDecoration.copyWith(
-                  hintText: 'Enter your email',
-                ),
-              ),
-              SizedBox(
-                height: 8.0,
-              ),
-              TextField(
-                textAlign: TextAlign.center,
-                obscureText: true,
-                onChanged: (value) {
-                  password = value;
-                },
-                style: TextStyle(color: Colors.black),
-                decoration: kTextFieldDecoration.copyWith(
-                  hintText: 'Enter your password',
-                ),
-              ),
-              SizedBox(
-                height: 24.0,
-              ),
-              RoundedButton(
-                color: Colors.lightBlueAccent,
-                title: 'Log In',
-                onPressed: () async {
-                  setState(() {
-                    _showspinner = true;
-                  });
-                  try {
-                    var session = await _auth.signInWithEmailAndPassword(
-                      email: email,
-                      password: password,
-                    );
-                    if (session != null) {
-                      print(session);
-                      // navigate to the chat screen
-                      Navigator.pushNamed(context, ChatScreen.id);
-                    }
+                  SizedBox(
+                    height: 48.0,
+                  ),
+                  TextField(
+                    textAlign: TextAlign.center,
+                    keyboardType: TextInputType.emailAddress,
+                    onChanged: (value) {
+                      email = value;
+                    },
+                    style: TextStyle(color: Colors.black),
+                    decoration: kTextFieldDecoration.copyWith(
+                      hintText: 'Enter your email',
+                    ),
+                  ),
+                  SizedBox(
+                    height: 8.0,
+                  ),
+                  TextField(
+                    textAlign: TextAlign.center,
+                    obscureText: true,
+                    onChanged: (value) {
+                      password = value;
+                    },
+                    style: TextStyle(color: Colors.black),
+                    decoration: kTextFieldDecoration.copyWith(
+                      hintText: 'Enter your password',
+                    ),
+                  ),
+                  SizedBox(
+                    height: 24.0,
+                  ),
+                  RoundedButton(
+                    color: Colors.lightBlueAccent,
+                    title: 'Log In',
+                    onPressed: () async {
+                      setState(() {
+                        _showspinner = true;
+                      });
+                      try {
+                        var session = await _auth.signInWithEmailAndPassword(
+                          email: email,
+                          password: password,
+                        );
+                        if (session != null) {
+                          print(session);
+                          // navigate to the chat screen
+                          Navigator.pushNamed(context, ChatScreen.id);
+                        }
 
-                    setState(() {
-                      _showspinner = false;
-                    });
-                  } on FireAuth.FirebaseAuthException catch (e) {
-                    if (e.code == 'wrong-password') {
-                      print('\n\nbad password!\n\n');
-                    }
-                    print('Error occurred during signin: $e');
-                  } catch (e) {
-                    print('Exception: code: ${e.code}, message: ${e.message}');
-                  }
-                },
+                        setState(() {
+                          _showspinner = false;
+                        });
+                      } on FireAuth.FirebaseAuthException catch (e) {
+                        setState(() {
+                          _showspinner = false;
+                        });
+                        if (e.code == 'wrong-password') {
+                          Scaffold.of(context).showSnackBar(
+                            SnackBar(
+                              backgroundColor: Colors.grey[900],
+                              content: Text(
+                                'Wrong password',
+                                textAlign: TextAlign.center,
+                                style: TextStyle(fontSize: 13),
+                              ),
+                            ),
+                          );
+                          print('\n\nbad password!\n\n');
+                        }
+                        print('Error occurred during signin: $e');
+                      } catch (e) {
+                        setState(() {
+                          _showspinner = false;
+                        });
+                        print(
+                            'Exception: code: ${e.code}, message: ${e.message}');
+                      }
+                    },
+                  ),
+                ],
               ),
-            ],
-          ),
-        ),
+            ),
+          );
+        },
       ),
     );
   }
